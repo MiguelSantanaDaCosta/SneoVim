18e3844c966e9e5efb979f99f2007a692442f9e3
MiguelSantanaDaCosta <miguelsantanadacosta@gmail.com>
2025-07-04 19:24:14 -0300

    first commit

---
+++ init.lua
@@ -0,0 +1,355 @@
+-- init.lua
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", lazypath
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- Dashboard (Alpha-nvim)
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
+      dashboard.section.header.val = {
+        [[ ███████╗ █████╗ ███╗   ██╗████████╗ █████╗ ███╗   ██╗ █████╗ ]],
+        [[ ██╔════╝██╔══██╗████╗  ██║╚══██╔══╝██╔══██╗████╗  ██║██╔══██╗]],
+        [[ ███████╗███████║██╔██╗ ██║   ██║   ███████║██╔██╗ ██║███████║]],
+        [[ ╚════██║██╔══██║██║╚██╗██║   ██║   ██╔══██║██║╚██╗██║██╔══██║]],
+        [[ ███████║██║  ██║██║ ╚████║   ██║   ██║  ██║██║ ╚████║██║  ██║]],
+        [[ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝]],
+        [[                                                              ]],
+        [[                      Bem-vindo, Santana                      ]],
+      }
+
+      dashboard.section.buttons.val = {
+        dashboard.button("e", "  > Explorer", ":NvimTreeToggle<CR>"),
+        dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+        dashboard.button("r", "  > Recent files", ":Telescope oldfiles<CR>"),
+        dashboard.button("s", "  > Session", ":Telescope session-lens search_session<CR>"),
+        dashboard.button("g", "  > Live grep", ":Telescope live_grep<CR>"),
+        dashboard.button("c", "  > Configuration", ":e ~/.config/nvim/init.lua<CR>"),
+        dashboard.button("q", "  > Quit", ":qa<CR>"),
+      }
+
+      alpha.setup(dashboard.config)
+    end,
+  },
+
+  -- Plugins de sessão (devem vir antes do telescope)
+  {
+    "rmagatti/auto-session",
+    config = function()
+      require("auto-session").setup({
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      })
+    end,
+  },
+
+  {
+    "rmagatti/session-lens",
+    dependencies = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("session-lens").setup({
+        path_display = { "shorten" },
+      })
+    end,
+  },
+
+  -- Sistema de Navegação
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+      "rmagatti/session-lens",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+            },
+            vertical = {
+              mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+          },
+        },
+        extensions = {
+          file_browser = {
+            theme = "dropdown",
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {},
+              ["n"] = {},
+            },
+          },
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+
+      telescope.load_extension("file_browser")
+      telescope.load_extension("ui-select")
+      telescope.load_extension("session-lens")
+
+      -- Atalhos
+      local map = vim.keymap.set
+      local builtin = require("telescope.builtin")
+      
+      map("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      map("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      map("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+      map("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+      map("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+      map("n", "<leader>fe", ":Telescope file_browser<CR>", { desc = "File explorer" })
+      map("n", "<leader>fs", ":Telescope session-lens search_session<CR>", { desc = "Search sessions" })
+    end,
+  },
+
+  -- LSP e Autocompletar
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      -- Configuração do Mason
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls", "clangd", "pyright", "jdtls", "bashls", "rust_analyzer"
+        },
+        automatic_installation = true,
+      })
+
+      -- Configuração do nvim-cmp
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+     
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- Configurações LSP por linguagem
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      })
+
+      -- C/C++
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=never",
+          "--all-scopes-completion",
+          "--completion-style=detailed",
+        },
+      })
+
+      -- Python
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+
+      -- Java
+      lspconfig.jdtls.setup({ capabilities = capabilities })
+
+      -- Bash
+      lspconfig.bashls.setup({
+        capabilities = capabilities,
+        filetypes = { "sh", "bash" },
+      })
+
+      -- Rust
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            checkOnSave = { command = "clippy" },
+          },
+        },
+      })
+
+      -- Atalhos LSP
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+        end,
+      })
+    end,
+  },
+
+  -- Outros plugins essenciais
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-tree/nvim-tree.lua", config = true },
+  { "lewis6991/gitsigns.nvim", config = true },
+  { "windwp/nvim-autopairs", config = true },
+  { "numToStr/Comment.nvim", config = true },
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  
+  -- Tema
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("gruvbox")
+    end,
+  },
+})
+
+-- Configurações globais do Neovim
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = truei
+vim.g.mapleader = " "  -- Tecla Espaço como leader
+vim.g.maplocalleader = " "  -- Leader para configurações locais
+-- Atalhos adicionais
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggle terminal" })
+
+-- Configuração para compilar e executar C++ com F5
+-- No final do seu init.lua (fora do bloco require("lazy").setup)
+local function compile_run_cpp()
+  local filename = vim.fn.expand('%:p')  -- Caminho completo do arquivo
+  local output = filename:gsub('.cpp$', '')  -- Remove extensão .cpp
+  
+  -- Verifica se o arquivo é .cpp
+  if not filename:match('%.cpp$') then
+    vim.notify("Este comando só funciona para arquivos .cpp", vim.log.levels.ERROR)
+    return
+  end
+
+  -- Comando de compilação + execução
+  local cmd = string.format('g++ -std=c++17 -Wall -Wextra %s -o %s && %s', 
+                          filename, output, output)
+  
+  -- Executa no terminal flutuante (toggleterm)
+  require('toggleterm').exec(cmd, 1, 15, nil, 'float')
+end
+
+-- Mapeamento do F5
+vim.keymap.set('n', '<F5>', function()
+  vim.cmd('w')  -- Salva o arquivo antes
+  compile_run_cpp()
+end, { desc = "Compilar e executar C++" })
